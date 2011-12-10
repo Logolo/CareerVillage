e86b20d3b842b8cb7b3e1a55b74741b795731736
Max Thayer <garbados@gmail.com>
2011-12-10 16:50:55 -0500

    Fixed install script.

--- install_cv.sh
+++ install_cv.sh
@@ -1,26 +1,26 @@
+project=$1
+user=$2
 # If you have easy_install, install pip using that
-$use = easy_install
-hash easy_install 2>&- || { $use = pip } 
-if [$use = easy_install]
+use=easy_install
+hash easy_install 2>&- || {$use = pip} 
+if [ $use = easy_install ]; then
 	sudo easy_install pip
 else
-	curl -O http://python-distribute.org/distribute_setup.py
-	python distribute_setup.py
+	wget http://python-distribute.org/distribute_setup.py
+	sudo python distribute_setup.py
+	rm distribute_setup.py
 fi
 # Now that we have pip, create the virtual environment
-$project = CareerVillage
 sudo pip install virtualenv
-virtualenv $project --no-site-packages
+virtualenv $project
 # activate the new virtualenv, and clone the git
 cd $project
 source bin/activate
-git clone git@github.com:jchubber/$project.git
+git clone git://github.com/$user/$project.git
 # enter project, install dependencies
 cd $project
 pip install -r requirements.txt
-# create local version of standard settings
-cp settings_local.py.dist settings_local.py
-# syncdb and migration. This should create a superuser as well.
-python manage.py syncdb --all 
-python manage.py migrate forum --fake
-# TODO CSS customization
+# manage git repos so we can push
+git remote rm origin
+git remote add origin git@github.com:$user/$project.git
+
