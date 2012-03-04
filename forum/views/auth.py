1e57ed36b0e9b5e480c7a62b47a5da21a3f35c1a
Jared Chung <v35m3lg234wb5@jostlr.com>
2012-03-04 04:10:20 -0500

    Fixes linkedin auth problem.

--- forum/views/auth.py
+++ forum/views/auth.py
@@ -197,7 +197,7 @@ def external_register(request):
             return HttpResponseRedirect(reverse('auth_signin'))
 
         provider_class = AUTH_PROVIDERS[auth_provider].consumer
-        user_data = provider_class.get_user_data(request.session['assoc_key'])
+        user_data = {} #provider_class.get_user_data(request.session['assoc_key']) #FIXED BY BEN WHEN LINKEDIN AUTH BROKE
 
         if not user_data:
             user_data = request.session.get('auth_consumer_data', {})
