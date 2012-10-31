cd1bfb321eed56b384dfbc82c759b80ad94743cc
Jared Chung <v35m3lg234wb5@jostlr.com>
2012-10-31 15:17:19 -0400

    Fixes whitespace problem in last commit.

--- forum/middleware/blocked_ips.py
+++ forum/middleware/blocked_ips.py
@@ -6,7 +6,7 @@ class BlockedIpMiddleware(object):
 
     def process_request(self, request):
         if request.META['HTTP_X_FORWARDED_FOR'] in settings.BLOCKED_IPS:
-        	logging.error("REQUEST FROM IP %s BLOCKED" % (request.META['HTTP_X_FORWARDED_FOR']))
+            logging.error("REQUEST FROM IP %s BLOCKED" % (request.META['HTTP_X_FORWARDED_FOR']))
             return http.HttpResponseForbidden('<h1>Forbidden</h1>')
         return None
 
