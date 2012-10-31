ca48fe6e9990276945eeb6feec0e0c565c6c0eb5
Jared Chung <v35m3lg234wb5@jostlr.com>
2012-10-31 14:59:15 -0400

    Adds logging to the IP blocking middleware.

--- forum/middleware/blocked_ips.py
+++ forum/middleware/blocked_ips.py
@@ -1,14 +1,15 @@
 from django.conf import settings
 from django import http
+import logging
 
 class BlockedIpMiddleware(object):
 
     def process_request(self, request):
-        print "Checking for BLOCKED_IPS"
+        logging.error("Checking %s for BLOCKED_IPS" % (request.META['REMOTE_ADDR']))
         if request.META['REMOTE_ADDR'] in settings.BLOCKED_IPS:
-            print "THIS IP IS BLOCKED"
+            logging.error("THIS IP IS BLOCKED")
             return http.HttpResponseForbidden('<h1>Forbidden</h1>')
-        print "This IP is valid."
+        logging.error("This IP is valid.")
         return None
 
 """
