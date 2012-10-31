07724b6a7db6c2f1331638d27ead766cc2c84752
Jared Chung <v35m3lg234wb5@jostlr.com>
2012-10-31 11:43:15 -0400

    Fixes whitespace bug.

--- forum/middleware/blocked_ips.py
+++ forum/middleware/blocked_ips.py
@@ -4,9 +4,9 @@ from django import http
 class BlockedIpMiddleware(object):
 
     def process_request(self, request):
-    	print "Checking for BLOCKED_IPS"
+        print "Checking for BLOCKED_IPS"
         if request.META['REMOTE_ADDR'] in settings.BLOCKED_IPS:
-        	print "THIS IP IS BLOCKED"
+            print "THIS IP IS BLOCKED"
             return http.HttpResponseForbidden('<h1>Forbidden</h1>')
         print "This IP is valid."
         return None
