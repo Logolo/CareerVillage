20727bb261af0e67d44d5c83cd57b7363ec35207
Jared Chung <v35m3lg234wb5@jostlr.com>
2012-10-31 15:04:40 -0400

    Changes IP block check to HTTP_X_FORWARDED_FOR from REMOTE_ADDR because WebFaction proxys all requests through a front-end server before it reaches the Django application.

--- forum/middleware/blocked_ips.py
+++ forum/middleware/blocked_ips.py
@@ -5,8 +5,8 @@ import logging
 class BlockedIpMiddleware(object):
 
     def process_request(self, request):
-        logging.error("Checking %s for BLOCKED_IPS" % (request.META['REMOTE_ADDR']))
-        if request.META['REMOTE_ADDR'] in settings.BLOCKED_IPS:
+        logging.error("Checking %s for BLOCKED_IPS" % (request.META['HTTP_X_FORWARDED_FOR']))
+        if request.META['HTTP_X_FORWARDED_FOR'] in settings.BLOCKED_IPS:
             logging.error("THIS IP IS BLOCKED")
             return http.HttpResponseForbidden('<h1>Forbidden</h1>')
         logging.error("This IP is valid.")
