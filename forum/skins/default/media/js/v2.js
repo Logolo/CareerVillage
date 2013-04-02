6c1ef10b28cbaf76a514068311dee7cf85a59bbb
Blaise Freeman <blaise.freeman@gmail.com>
2013-04-02 13:14:35 -0400

    wrap question to be better resize and include follow button

--- forum/skins/default/media/js/v2.js
+++ forum/skins/default/media/js/v2.js
@@ -2,5 +2,14 @@
 // any global variables should be declared avobe this comment, not that we should be using global variables...
 $(function(){
     
+    // trigger animations when the like question button is clicked
+    $(".like-question-button").click(function(e){
+        e.preventDefault();
+                
+        // reveal the follow question button
+        $('.follow-question').animate({'opacity': '1'});
+
+
+    });
 
 });
