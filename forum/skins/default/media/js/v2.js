f6fcd55f4a73d037bdf2f48be58725b5f7473d29
Blaise Freeman <blaise.freeman@gmail.com>
2013-04-03 17:28:31 -0400

    updates to like widget (not 100% complete)

--- forum/skins/default/media/js/v2.js
+++ forum/skins/default/media/js/v2.js
@@ -2,31 +2,56 @@
 // any global variables should be declared avobe this comment, not that we should be using global variables...
 $(function(){
     
-    // trigger animations when the like question button is clicked
-    $(".like-question-button").click(function(e){
-        e.preventDefault();
 
-        // prevent multiple likes
-        $(this).addClass('disabled');                 
 
-        // like page
-        url = $(this).attr('href');
-        _this = this;
-        $.ajax({
-          url: url,
-          data: ''
-        }).success(function(e){
-                $(this).addClass('btn-primary disabled').removeClass('btn-success').text('Liked!');
-                $num = $(this).closest('.like-widget').find('h3');
-                $num.text(parseInt($num.text())+1);            
-                // reveal the follow question button
-                $('.follow-question').animate({'opacity': '1'});
-        }).done(function() {    
-            $(this).removeClass('disabled'); 
-        });
+
+/*** 
+ * trigger animations when the like button is clicked
+ */
+
+// trigger animations when the like question button is clicked
+$(".like-question-button").click(function(e){
+    e.preventDefault();
+
+    // disable the button prevent multiple likes
+    $(this).addClass('disabled');                 
+
+    $widget = $(this).closest('.like-widget');
+    $follow = $widget.find('.follow-question');
+
+    // are we liking or unliking
+    var liking = ($widget.hasClass('on'))? true : false;
+    var $likes = $widget.find('h3');
+
+    // like ajax request
+    $.getJSON($(this).attr('href'), function(data, e) {
+
+        // re-enable the button
+        $(this).removeClass('disabled');
+
+        // respond to error
+        if (!data.success && data['error_message'] != undefined) {
+            console.log(data.error_message);
+            return;
+        } 
+        
+        // increase/decrease likes count
+        if (liking) {
+            $likes.text(parseInt($likes.text())+1);
+        } else {
+            $likes.text(parseInt($likes.text())-1);
+        }
+
+        // show/hide the follow button if exists in this context
+        if ($follow.length) {
+            if (liking) {
+                $('.follow-question').animate({'opacity': 1});
+            } else {
+                $('.follow-question').animate({'opacity': .2});
+            }            
+        }
 
     });
-
 });
 
 
@@ -54,3 +79,8 @@ function beforeUnload(e) {
 }
 window.addEventListener('beforeunload', beforeUnload, true);
 */
+
+
+
+
+}); // end jquery enclosure
