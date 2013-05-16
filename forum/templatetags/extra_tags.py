86c6fad467fe82055797e4a6e89b1de951003225
Eduardo Veiga <ed.cinemascop@yahoo.es>
2013-05-16 18:49:29 -0300

    anonymous users can go to /home, closes #269

--- forum/templatetags/extra_tags.py
+++ forum/templatetags/extra_tags.py
@@ -45,20 +45,27 @@ def gravatar(user, size):
 @register.simple_tag
 def avatar(user, size, css_class=''):
     img_template = '<img class="%(class)s" width="%(size)s" height="%(size)s" src="%(src)s">'
-    if user.linkedin_photo_url:
-        avatar_tag = img_template % {
-            'src': user.linkedin_photo_url,
-            'size': size,
-            'class': css_class,
-        }
-    elif user.avatar_image:
-        avatar_tag = img_template % {
-            'src': media("/media/img/example_avatars/%s" % (user.avatar_image,)),
-            'size': size,
-            'class': css_class,
-        }
+    if user.is_authenticated():
+        if user.linkedin_photo_url:
+            avatar_tag = img_template % {
+                'src': user.linkedin_photo_url,
+                'size': size,
+                'class': css_class,
+            }
+        elif user.avatar_image:
+            avatar_tag = img_template % {
+                'src': media("/media/img/example_avatars/%s" % (user.avatar_image,)),
+                'size': size,
+                'class': css_class,
+            }
+        else:
+            avatar_tag = gravatar(user, size)
     else:
-        avatar_tag = gravatar(user, size)
+        avatar_tag = img_template % {
+            'src': media("/media/images/nophoto.png"),
+            'size': size,
+            'class': css_class,
+        }
 
     return avatar_tag
 
