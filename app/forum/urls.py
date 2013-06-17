0d2910df4d16946165c16d4ec3a2167554cc6d41
Jared Chung <v35m3lg234wb5@jostlr.com>
2013-06-17 14:50:55 -0400

    Creates the first version of the /advice/ page. Content needs to change and get restyled.

--- app/forum/urls.py
+++ app/forum/urls.py
@@ -58,6 +58,9 @@ core_urls = (
     url(r'^%s%s$' % (_('questions/'), _('unanswered/')), app.readers.unanswered, name='unanswered'),
     url(r'^%s(?P<mode>[\w\-]+)/(?P<user>\d+)/(?P<slug>.*)/$' % _('questions/'), app.readers.user_questions, name='user_questions'),
 
+    ## FOR NCVS CONFERENCE
+    url(r'^%s$' % _('advice/'), direct_to_template, {'template': 'advice_recruiting.html'}),
+
     url(r'^%s(?P<id>\d+)/%s$' % (_('questions/'), _('edit/')), app.writers.edit_question, name='edit_question'),
     url(r'^%s(?P<id>\d+)/(?P<slug>.*)/%s$' % (_('questions/'), _('edit/')), app.writers.edit_question_v2, name='edit_question_v2'),
     url(r'^%s(?P<id>\d+)/%s$' % (_('questions/'), _('close/')), app.commands.close, kwargs=dict(close=True), name='close'),
